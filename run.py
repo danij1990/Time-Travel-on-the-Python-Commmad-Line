85f2b84bb431c1b377343002585bdc4019b9560d
danij1990 <>
2023-09-29 14:10:06 +0000

    Inital Commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random  # Importing the random module for generating random choices
+
+
+# Function for the main time travel game
+def time_travel():
+    # Printing welcome and instructions
+    print("Welcome to the Time Travel Game!")
+    print("You find yourself in a mysterious time machine.")
+    print("You need to answer all the questions and riddles")
+    print("to escape the time warp.")
+    print("Try to escape the loop and also have fun.")
