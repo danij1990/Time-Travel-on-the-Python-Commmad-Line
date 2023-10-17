d54da4af4ddf335bf382bce4a8192ef72eccebdf
danij1990 <>
2023-10-17 15:56:28 +0000

    change the question loop

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
-import random  # Importing the random module for generating random choices
+import random
 
-# Validate and get a valid Name
-while True:
-    name = input("Enter your name: ")
-
-    if name.isalpha():
-        break
-    else:
-        print("Please enter a valid name with alphabetic characters only.")
+name = input("Enter your name: ")
 
 # Validate and get a valid age
 while True:
@@ -22,176 +15,88 @@ while True:
 print(f"Hello {name}! You are {age} years old.")
 
 
+# Function to ask a question with multiple choices
+def ask_question(question, choices, correct_choice):
+    print(question)
+    for i, choice in enumerate(choices, start=1):
+        print(f"{chr(64 + i)}. {choice}")
 
-import random  # Importing the random module for generating random choices
+    user_choice = input("Your choice (A/B/C/D): ").upper()
+    return user_choice == correct_choice
 
 
 # Function for the main time travel game
 def time_travel():
-    # Printing welcome and instructions
     print("Welcome to the Time Travel Game!")
     print("You find yourself in a mysterious time machine.")
     print("You need to answer all the questions and riddles")
     print("to escape the time warp.")
     print("Try to escape the loop and also have fun.")
 
-    # Flag to track if all questions are answered
-    answered_all_questions = False
-
-    # Loop to play the game until all questions are answered
-    while not answered_all_questions:
-        options = {
-            1: "Go to the future",
-            2: "Go back in time",
-            3: "Stay in the present day",
-            4: "Your Birthday, but which one?"
+    questions = [
+        {
+            "question": "What goes up but must come down?",
+            "choices": ["A Plane", "A Cat", "A Dog", "A Balloon"],
+            "correct_choice": "A",
+            "adventure_route": "You see a time portal resembling a floating airplane. You board it and embark on a journey to a futuristic city."
+        },
+        {
+            "question": "What belongs to you, but other people use it more than you do?",
+            "choices": ["Your Heart", "Your Name", "Your Money", "Your House"],
+            "correct_choice": "B",
+            "adventure_route": "You notice a time portal leading to your house. As you enter, you find a hidden treasure in your backyard."
+        },
+        {
+            "question": "What is the world's largest mountain?",
+            "choices": ["Everest", "K2", "Mount Fuji", "Makalu"],
+            "correct_choice": "A",
+            "adventure_route": "You spot a time portal leading to the highest peak. You climb and discover a hidden cave with ancient artifacts."
+        },
+        {
+            "question": "What has keys but can't open locks?",
+            "choices": ["Map", "Piano", "Glasses", "Cryptography"],
+            "correct_choice": "B",
+            "adventure_route": "You come across a time portal resembling a grand piano. As you approach, the piano door opens, revealing a room with a treasure chest."
+        },
+        {
+            "question": "What is always in front of you but can't be seen?",
+            "choices": ["The Future", "The Past", "The Present", "Your Nose"],
+            "correct_choice": "C",
+            "adventure_route": "You find a time portal surrounded by mist. As you walk through it, you enter a hidden garden full of wonders."
         }
-        for key, value in options.items():
-            print(f"{key}: {value}")
+    ]
 
-        choice = input("Enter your choice: ")
+    # List to track if each question is answered correctly
+    correct_answers = [False] * len(questions)
 
-        # Checking the user's choice and calling corresponding functions
-        if choice == '1':
-            print("You have landed on a spaceship.")
-            explore_future()
-        elif choice == '2':
-            print("You go deeper into the forest and encounter an animal.")
-            encounter_past()
-        elif choice == '3':
-            print("You are whisked off to a mountain range.")
-            today()
-        elif choice == '4':
-            print("You have landed at your 90th birthday party.")
-            your_birthday()
-        else:
-            print("Invalid choice. Please try again.")
+    # Loop to play the game
+    while not all(correct_answers):
+        for i, question_info in enumerate(questions, start=1):
+            print(f"\nYou encounter a time portal that reads: '{question_info['adventure_route']}'")
+            print(f"What do you want to do for question {i}?")
+            print("A. Enter the time portal")
+            print("B. Skip and move to the next question")
+            
+            portal_choice = input("Your choice (A/B): ").upper()
+
+            if portal_choice == 'A':
+                correct_answers[i-1] = ask_question(question_info["question"], question_info["choices"], question_info["correct_choice"])
+
+                # If the answer is wrong, end the game and start over
+                if not correct_answers[i-1]:
+                    print("Oops! You answered incorrectly. Starting over.")
+                    break
+
+            elif portal_choice == 'B':
+                print("You decide to skip the time portal and move to the next question.")
+                break
+
+            else:
+                print("Invalid choice. Game over.")
+                return
 
     # Printing congratulations when the game is completed
-    print("Congratulations! all answers are correct.")
-
-
-# Function to explore the future
-def explore_future():
-    print("You find a safe on the spaceship.")
-    print("Do you want to open the safe?")
-    print("A. Yes")
-    print("B. No")
-    print("C. Continue to search")
-
-    choice = input("Enter your choice (A/B/C): ")
-
-    if choice.lower() == 'a':
-        if ask_question1("What goes up but must come down?", "A"):
-            print("Congratulations! You found a valuable treasure.")
-        else:
-            print("You answered the riddle incorrectly. Game over.")
-    elif choice.lower() == 'b':
-        print("You leave the ship and continue your time-traveling adventure.")
-    elif choice.lower() == 'c':
-        print("You found an old book, It has some interesting stories.")
-    else:
-        print("Invalid choice. Please try again.")
-
-
-# Function to encounter the past
-def encounter_past():
-    print("You have landed beside a fierce dinosaur!")
-    print("What do you want to do?")
-    print("A. Fight the animal")
-    print("B. Try to run away")
-
-    choice = input("Enter your choice (A/B): ")
-
-    if choice.lower() == 'a':
-        if ask_question2("What belongs to you, but other people use it more than you do?", "B"):
-            print("You fought the animal and managed to defeat it.")
-        else:
-            print("You answered the riddle incorrectly. Game over.")
-    elif choice.lower() == 'b':
-        print("You try to run away, but the animal catches you.")
-        print("Game over. You were not able to escape.")
-
-
-# Function for the "stay in the present day" option
-def today():
-    print("You are near the top of a mountain!")
-    print("What do you want to do?")
-    print("A. Keep Climbing")
-    print("B. Try to run away")
-
-    choice = input("Enter your choice (A/B): ")
-
-    if choice.lower() == 'a':
-        if ask_question3("What is the world's largest mountain?", "A"):
-            print("You reached the top of Everest.")
-        else:
-            print("You answered the riddle incorrectly. Game over.")
-    elif choice.lower() == 'b':
-        print("You had to be rescued by the locals.")
-        print("Game over. You were not able to escape.")
-
-
-# Function for the "your birthday" option
-def your_birthday():
-    print("Would you rather")
-    print("A. Stay at the party?")
-    print("B. Choose to return home?")
-
-    choice = input("Enter your choice (A/B): ")
-
-    if choice.lower() == 'a':
-        if ask_question4("What has keys but can't open locks?", "B"):
-            print("Enjoy your party.")
-        else:
-            print("You answered the riddle incorrectly. Game over.")
-    elif choice.lower() == 'b':
-        print("The party is over.")
-        print("Game over. You were not able to escape.")
-
-
-# Question function 1 to ask a question with multiple choices
-def ask_question1(question, correct_choice):
-    print(question)
-    print("Choices:")
-    print("A. A Plane")
-    print("B. A Cat")
-    print("C. A Dog")
-    user_choice = input("Your choice (A/B/C): ").lower()
-    return user_choice == correct_choice.lower()
-
-
-# Question function 2 to ask a question with multiple choices
-def ask_question2(question, correct_choice):
-    print(question)
-    print("Choices:")
-    print("A. Your Heart")
-    print("B. Your Name")
-    print("C. Your Money")
-    user_choice = input("Your choice (A/B/C): ").lower()
-    return user_choice == correct_choice.lower()
-
-
-# Question function 3 to ask a question with multiple choices
-def ask_question3(question, correct_choice):
-    print(question)
-    print("Choices:")
-    print("A. Everest")
-    print("B. K2")
-    print("C. Mount Fuji")
-    user_choice = input("Your choice (A/B/C): ").lower()
-    return user_choice == correct_choice.lower()
-
-
-# Question function 4 to ask a question with multiple choices
-def ask_question4(question, correct_choice):
-    print(question)
-    print("Choices:")
-    print("A. Map")
-    print("B. Piano")
-    print("C. Glasses")
-    user_choice = input("Your choice (A/B/C): ").lower()
-    return user_choice == correct_choice.lower()
+    print("You have escaped the loop! Well done!")
 
 
 if __name__ == "__main__":
